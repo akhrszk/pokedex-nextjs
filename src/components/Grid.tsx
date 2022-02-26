c6ee26970a20ea9799389adb1b0fc58a00247fdd
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2022-02-27 04:20:30 +0900

    不要なファイル削除

--- src/components/Grid.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Grid: React.FC = props => (
-  <div className="max-w-3xl flex flex-wrap gap-4 justify-center">
-    {props.children}
-  </div>
-)
-
-export default Grid
